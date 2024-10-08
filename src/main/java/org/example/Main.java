c8fa807d13ed0c795f63c5d9c02d07f04adba9e0
SeeXWH <mihaandre1@yamdex.ru>
2024-10-08 22:16:36 +0300

    clean repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.example.linkedList.CustomLinkedList;
 
 
 import java.io.File;
@@ -11,42 +10,15 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+
+
 @Slf4j
 public class Main {
 
 
     public static void main(String[] args) {
         lesson2();
-        lesson3();
     }
-
-    public static void lesson3() {
-        CustomLinkedList<Integer> list = new CustomLinkedList<>();
-        list.add(1);
-        System.out.println("Get (0): " + list.get(0)); // 1
-
-        list.remove(0);
-        list.add(2);
-        list.add(3);
-        System.out.println("Contains (2): " + list.contains(2)); // true
-
-        List<Integer> newList = Arrays.asList(4, 5, 6);
-        list.addAll(newList);
-
-        System.out.print("List contents: ");
-        for (int i = 0; i < list.getSize(); i++) {
-            System.out.print(list.get(i) + " "); // 2 3 4 5 6
-        }
-
-
-        Stream<Integer> stringStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        CustomLinkedList<Integer> linkedList = CustomLinkedList.fromStream(stringStream);
-        System.out.println(linkedList.toString());
-    }
-
 
     public static void lesson2() {
         log.debug("Application started");
