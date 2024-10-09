d2af0e78804f1a87fd5a3d90d0f52229a750d16e
SeeXWH <mihaandre1@yamdex.ru>
2024-10-09 09:57:17 +0300

    edit gitignore

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
         lesson2();
+        lesson3();
     }
 
     public static void lesson2() {
@@ -49,5 +50,27 @@ public class Main {
             }
         }
         log.debug("Application ended");
+    }
+    public static void lesson3() {
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+        list.add(1);
+        System.out.println("Get (0): " + list.get(0)); // 1
+
+        list.remove(0);
+        list.add(2);
+        list.add(3);
+        System.out.println("Contains (2): " + list.contains(2)); // true
+
+        List<Integer> newList = Arrays.asList(4, 5, 6);
+        list.addAll(newList);
+
+        System.out.print("List contents: ");
+        for (int i = 0; i < list.getSize(); i++) {
+            System.out.print(list.get(i) + " "); // 2 3 4 5 6
+        }
+
+        Stream<Integer> stringStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        CustomLinkedList<Integer> linkedList = CustomLinkedList.fromStream(stringStream);
+        System.out.println(linkedList.toString());
     }
 }
